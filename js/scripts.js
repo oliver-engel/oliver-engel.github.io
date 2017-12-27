0b03d369e1d301fa602dc109aa9bcdbf42827b53
oliverengel <oliverengel6@gmail.com>
2017-12-26 19:43:26 -0800

    d

--- js/scripts.js
+++ js/scripts.js
@@ -27,13 +27,9 @@ $(document).ready(function() {
 
 });
 
-$(window).on("load", function() {
-	$('#return-to-top').click(function() {      // When arrow is clicked
-        $('body,html').animate({
-            scrollTop : 0                       // Scroll to top of body
-        }, 1015);
-    });
-
+$('body').on('pageinit', function() {
+  scrollTop();
+	LazyLoad();
 
 });
 
