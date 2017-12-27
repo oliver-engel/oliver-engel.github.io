a9166b5d561f6235d677c8e110b8f9cbdc40c579
oliverengel <oliverengel6@gmail.com>
2017-12-26 19:17:33 -0800

    g

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,11 @@
 $(document).ready(function() {
 
 	"use strict";
+	
+	LazyLoad();
 
-  accordion();
+  toggler();
+	rotater();
 	FirstLoad();
 	HeroSection();
 	AjaxLoad();
@@ -25,7 +28,7 @@ $(document).ready(function() {
 
 $(window).on("load", function() {
   scrollTop();
-	LazyLoad();
+
 
 });
 
@@ -41,26 +44,45 @@ function scrollTop(){
     });
 }
 
-function accordion(){
-	var acc = document.getElementsByClassName("accordion");
-	var i;
 
-	for (i = 0; i < acc.length; i++) {
-	    acc[i].addEventListener("click", function() {
-	        /* Toggle between adding and removing the "active" class,
-	        to highlight the button that controls the panel */
-	        this.classList.toggle("active");
+/*--------------------------------------------------
+Function toggler
+---------------------------------------------------*/
 
-	        /* Toggle between hiding and showing the active panel */
-	        var panel = this.nextElementSibling;
-	        if (panel.style.display === "block") {
-	            panel.style.display = "none";
-	        } else {
-	            panel.style.display = "block";
-	        }
-	    });
-	}
+function toggler(){
+	$("#toggler-challenge").click(function(){
+		$("#div-challenge").slideToggle();
+	});
+
+
+	$("#toggler-research").click(function(){
+		$("#div-research").slideToggle();
+	});
+
+	$("#toggler-ideation").click(function(){
+		$("#div-ideation").slideToggle();
+	});
+
+	$("#toggler-testing").click(function(){
+		$("#div-testing").slideToggle();
+	});
+
+	$("#toggler-final").click(function(){
+		$("#div-final").slideToggle();
+	});
 }
+
+/*--------------------------------------------------
+Function rotater
+---------------------------------------------------*/
+
+function rotater(){
+	$(".rotate").click(function(){
+		$(this).toggleClass("down");
+	})
+
+}
+
 
 
 /*--------------------------------------------------
