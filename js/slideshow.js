aad61eedf5161140961c0d6a22465cd6d525f0b9
Oliver Engel <oliverengel6@gmail.com>
2019-03-24 19:01:41 -0700

    updates

--- js/slideshow.js
+++ js/slideshow.js
@@ -2,29 +2,23 @@
 Slideshow
 ---------------------------------------------------*/
 
-	function currentDiv(n) {
-	  showDivs(slideIndex = n);
-	}
 
-	var slideIndex = 1;
-	showDivs(slideIndex);
+function currentDiv(n, whichSlideSet) {
+  showDivs(slideIndex = n, whichSlideSet);
+}
 
-	function plusDivs(n) {
-	  showDivs(slideIndex += n);
-	}
+function showDivs(n, whichSlideSet) {
+  var i;
+  var x = document.getElementsByClassName(whichSlideSet);
 
-	function showDivs(n) {
-	  var i;
-	  var x = document.getElementsByClassName("mySlides");
-	  var dots = document.getElementsByClassName("demo");
-	  if (n > x.length) {slideIndex = 1}
-	  if (n < 1) {slideIndex = x.length}
-	  for (i = 0; i < x.length; i++) {
-	    x[i].style.display = "none";
-	  }
-	  for (i = 0; i < dots.length; i++) {
-	    dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
-	  }
-	  x[slideIndex-1].style.display = "block";
-	  dots[slideIndex-1].className += " w3-opacity-off";
-	}
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+
+  x[slideIndex-1].style.display = "block";
+  // dots[slideIndex-1].className += " w3-opacity-off";
+
+
+}
