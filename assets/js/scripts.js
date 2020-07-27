3df3344bee2750a3fc20a832c8ded71f980abddc
Oliver Engel <33558453+oliver-engel@users.noreply.github.com>
2020-07-27 14:48:33 -0700

    asdf

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,6 +4,26 @@ jQuery(document).ready(function($) {
 
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+
+  try {
+
+    MicroModal.init({
+      awaitCloseAnimation: true,// set to false, to remove close animation
+      onShow: function(modal) {
+        console.log("micromodal open");
+      },
+      onClose: function(modal) {
+        console.log("micromodal close");
+      }
+    });
+
+  } catch (e) {
+    console.log("micromodal error: ", e);
+  }
+
+});
+
 var imgCounter = 0;
 
 function nextImage(){
